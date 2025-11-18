36f1ec3870835486fff80d37efc5648d4e7b32a5
imadiz <weszerle42@gmail.com>
2025-11-18 14:46:27 +0100

    Minden is

--- 10K/Python/alapok.py
+++ 10K/Python/alapok.py
@@ -1,18 +1,23 @@
 #Szöveg (string), Egész szám (int), Valós szám (float), Logikai (boolean), Lista (list)
 
-egesz = 1
-tort = 1.5
-szoveg = 'Hello'
-logikai = False
-lista = [1, 1.5, "Szöveg", True]
 
+elso = 10
+masodik = 12123456789
+abc = 'Ez egy szöveg!'
+abc2 = "Ez egy másik szöveg!"
 a = 5
-b = 6
+b = 2
 
-print(5+6)
-print(5-6)
-print(5*6)
-print(5/6)
-print(5**6)
-print(6//5)
-print(6%2)
+print(a, b)
+print(a+b)
+print(a-b)
+print(a*b) #Szorzás
+print(a/b) #Osztás
+print(a**b) #Hatványozás
+print(a//b) #Egész osztás
+print(a%b) #Maradékos osztás (modulo)
+
+szamok = [1, 3, 5, 7, 11, 547]
+print(szamok[0])
+print(szamok[5])
+print(szamok[-1])
