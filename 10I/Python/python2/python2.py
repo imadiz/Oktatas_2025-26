cc68cc05c3736a6402d4f701d56cde3c838a8223
imadiz <weszerle42@gmail.com>
2026-03-16 10:15:05 +0100

    313 pull-push

--- 10I/Python/python2/python2.py
+++ 10I/Python/python2/python2.py
@@ -1,52 +1,4 @@
 #--------------------------
-'''
-    A benne_van_a_stringben nevű függvény.
-    első paraméterként egy stringet kap,
-    második paraméterként egy betüt.
-    Visszatérési értéke True, ha  a betü benne van a stringben.
-    A visszatérési érték False, ha  a betü nics benne a stringben.
-'''
-def benne_van_a_stringben(sztring,betu):
-    if len(sztring) == 0:
-        return False
-    for n in sztring:
-        if n in betu:
-            return True
-
-    return False
-
-
-        
-    
-
-
-
-#--------------------------
-'''
-    A faktorialis nevű függvény,
-    visszatér a paraméterként megkapott szám faktoriálisával.
-'''
-def faktorialis(szam):
-    szamolas = 1
-    if szam < 0:
-        return None
-    elif szam == 0:
-        return 1
-    for i in range(1, szam+1):
-        szamolas = szamolas * i
-        
-        print(szamolas, "*", i)
-        print(szamolas)
-    return szamolas
-
-#assert faktorialis(0) == 1
-#assert faktorialis(1) == 1
-#assert faktorialis(5) == 120
-#assert faktorialis(10) == 3628800
-#assert faktorialis(-1) ==None # Negatív számot nem fogad el
-
-
-#--------------------------
 ''' 
     A szorzat  nevű függvény,
     paraméterként egy számokat tartalmazó listát kap és
@@ -54,42 +6,11 @@ def faktorialis(szam):
     
     Üres lista esetén 1 a visszatérési érték.
 '''
-
 def szorzat(lista):
-    if lista == []:
-        return 1
-    szamolas = 1
+    ered = 1
     for i in lista:
-        szamolas *= i
-    return szamolas
-    
-
-#--------------------------
-''' 
-    A legkisebb nevű függvény,
-    paraméterként egy számokat tartalmazó listát kap és
-    visszatér a lista legkisebb számával.
-    Üres lista esetén None a visszatérési érték.
-    
-    A feladat megoldása során nem használhatod a min() függvényt!
-'''
-
-def legkisebb(lista):
-    if len(lista) == 0:return None
-    kicsi = lista[0]
-    for i in lista:
-        if i < kicsi:
-            kicsi = i
-    return kicsi
-
-#--------------------------
-'''
-    A negativok_kivalogatasa nevű függvény,
-    visszatér egy listával, 
-      amely a paraméterként átadott számokat tartalmazó lista
-      negatív számait tartalmazza.
-'''
-
+        ered *= i
+    return ered
 
 
 #--------------------------
@@ -100,71 +21,109 @@ def legkisebb(lista):
     Üres lista esetén 0 a visszatérési érték.
     A feladat megoldása során nem használhatod a sum() függvényt!
 '''
-
-
-
-#--------------------------
-'''
-    A paratlanok_kivalogatasa nevű függvény,
-    visszatér egy listával, 
-        amely a paraméterként átadott számokat tartalmazó lista
-        páratlan számait tartalmazza.
-'''
-
+def osszeg(lista):
+    osszeg = 0
+    for szam in lista:
+        osszeg += szam
+    return osszeg
 
 
 #--------------------------
 ''' 
-    A negativok_szama nevű függvény,
+    A legkisebb nevű függvény,
     paraméterként egy számokat tartalmazó listát kap és
-    visszatér egy számokat tartalmazó lista negativ számainak számával.
+    visszatér a lista legkisebb számával.
+    Üres lista esetén None a visszatérési érték.
     
-    Üres lista esetén 0 a visszatérési érték.
+    A feladat megoldása során nem használhatod a min() függvényt!
 '''
+def legkisebb(lista):
+    if len(lista) == 0:
+        return None
+    minimum = lista[0]
+    for i in lista:
+        if i < minimum:
+            minimum = i
+    return minimum
 
 
 
 #--------------------------
+''' 
+    Az utolso_karakter nevű függvény,
+    paraméterként egy stringet kap és
+    visszatér az adott string utolso karakterével.
+    
+    Üres string esetén None a visszatérési érték.
 '''
-    A parosok_kivalogatasa nevű függvény,
+def utolso_karakter(sztring):
+    if sztring == "":
+        return None
+    return sztring[-1]
+
+
+#--------------------------
+'''
+    A negativok_kivalogatasa nevű függvény,
     visszatér egy listával, 
-        amely a paraméterként átadott számokat tartalmazó lista
-        páros számait tartalmazza.
+      amely a paraméterként átadott számokat tartalmazó lista
+      negatív számait tartalmazza.
 '''
-
-
-
-#--------------------------
-''' 
-    A pozitivok_szama nevű függvény,
-    paraméterként egy számokat tartalmazó listát kap és
-    visszatér egy számokat tartalmazó lista pozitív számainak számával.
-    
-    Üres lista esetén 0 a visszatérési érték.
-'''
-
-
-
-#--------------------------
-''' 
-    A parosok_szama  nevű függvény,
-    visszatér egy számokat tartalmazó lista páros számainak számával.
-    
-    Üres lista esetén 0 a visszatérési érték.
-'''
-
-
+def negativok_kivalogatasa(lista):
+    neg = []
+    for i in lista:
+        if i < 0:
+            neg.append(i)
+    return neg
 
 #--------------------------
 '''
-    A kereses_a_stringben nevű függvény,
-    első paraméterként egy sztringet kap,
+    A benne_van_a_stringben nevű függvény.
+    első paraméterként egy stringet kap,
     második paraméterként egy betüt.
-    Visszatérési érték a paraméterként megadott betü
-        első előfordulási helye a stringben. 
-    
-    A visszatérési érték None, ha a betü nics benne a stringben.
+    Visszatérési értéke True, ha  a betü benne van a stringben.
+    A visszatérési érték False, ha  a betü nics benne a stringben.
 '''
+
+
+
+#--------------------------
+''' 
+    A lista_atlag nevű függvény,
+    paraméterként egy számokat tartalmazó listát kap és
+    visszatér a lista számainak átlagával.
+'''
+
+
+
+#--------------------------
+'''
+    A faktorialis nevű függvény,
+    visszatér a paraméterként megkapott szám faktoriálisával.
+'''
+def faktorialis(n):
+    if n >= 0:
+        eredmeny = 1
+        for i in range(1, n+1):
+            eredmeny *= i
+        return eredmeny
+    else:
+        return None
+
+
+#--------------------------
+''' 
+    A pozitivok_kivalogatasa nevű függvény,
+    visszatér egy listával, 
+        amely a paraméterként átadott számokat tartalmazó lista
+        pozitiv számait tartalmazza. 
+'''
+def pozitivok_kivalogatasa(lista):
+    pozi = []
+    for i in lista:
+        if i > 0:
+            pozi.append(i)
+    return pozi
 
 
 
@@ -195,11 +154,87 @@ def legkisebb(lista):
 
 #--------------------------
 ''' 
-    Az utolso_karakter nevű függvény,
-    paraméterként egy stringet kap és
-    visszatér az adott string utolso karakterével.
+    A negativok_szama nevű függvény,
+    paraméterként egy számokat tartalmazó listát kap és
+    visszatér egy számokat tartalmazó lista negativ számainak számával.
     
-    Üres string esetén None a visszatérési érték.
+    Üres lista esetén 0 a visszatérési érték.
+'''
+def negativok_szama(lista):
+    if lista== []:
+        return 0
+    neg= 0
+    for i in lista:
+        if i < 0:
+            neg+=1 
+    return neg        
+
+    
+
+
+#--------------------------
+'''
+    A paratlanok_kivalogatasa nevű függvény,
+    visszatér egy listával, 
+        amely a paraméterként átadott számokat tartalmazó lista
+        páratlan számait tartalmazza.
+'''
+
+
+
+#--------------------------
+''' 
+    A parosok_szama  nevű függvény,
+    visszatér egy számokat tartalmazó lista páros számainak számával.
+    
+    Üres lista esetén 0 a visszatérési érték.
+'''
+
+
+
+#--------------------------
+'''
+    A kereses_a_stringben nevű függvény,
+    első paraméterként egy sztringet kap,
+    második paraméterként egy betüt.
+    Visszatérési érték a paraméterként megadott betü
+        első előfordulási helye a stringben. 
+    
+    A visszatérési érték None, ha a betü nics benne a stringben.
+'''
+
+
+
+#--------------------------
+'''
+    A parosok_kivalogatasa nevű függvény,
+    visszatér egy listával, 
+        amely a paraméterként átadott számokat tartalmazó lista
+        páros számait tartalmazza.
+'''
+
+
+
+#--------------------------
+''' 
+    A pozitivok_szama nevű függvény,
+    paraméterként egy számokat tartalmazó listát kap és
+    visszatér egy számokat tartalmazó lista pozitív számainak számával.
+    
+    Üres lista esetén 0 a visszatérési érték.
+'''
+
+
+
+#--------------------------
+'''
+    A benne_van_a_listaban nevű függvény,
+    első paraméterként egy listát kap,
+    második paraméterként egy számot.
+    A visszatérési érték True, ha  a szám benne van a listában.
+    A visszatérési érték False, ha  a szám nics benne a listában.
+    
+    Üres lista esetén a visszatérési érték False.
 '''
 
 
@@ -217,43 +252,11 @@ def legkisebb(lista):
 
 #--------------------------
 ''' 
-    A lista_atlag nevű függvény,
-    paraméterként egy számokat tartalmazó listát kap és
-    visszatér a lista számainak átlagával.
-'''
-
-
-
-#--------------------------
-''' 
-    A pozitivok_kivalogatasa nevű függvény,
-    visszatér egy listával, 
-        amely a paraméterként átadott számokat tartalmazó lista
-        pozitiv számait tartalmazza. 
-'''
-
-
-
-#--------------------------
-''' 
     Az elso_karakter nevű függvény,
     paraméterként egy stringet kap és
     visszatér a string első karakterével.
     
     Üres string esetén None a visszatérési érték.
-'''
-
-
-
-#--------------------------
-'''
-    A benne_van_a_listaban nevű függvény,
-    első paraméterként egy listát kap,
-    második paraméterként egy számot.
-    A visszatérési érték True, ha  a szám benne van a listában.
-    A visszatérési érték False, ha  a szám nics benne a listában.
-    
-    Üres lista esetén a visszatérési érték False.
 '''
 
 
---
+++ 10K/Python/python2-gyak/python2.py
@@ -0,0 +1,236 @@
+#--------------------------
+'''
+    A kereses_a_stringben nevű függvény,
+    első paraméterként egy sztringet kap,
+    második paraméterként egy betüt.
+    Visszatérési érték a paraméterként megadott betü
+        első előfordulási helye a stringben. 
+    
+    A visszatérési érték None, ha a betü nics benne a stringben.
+'''
+
+
+
+#--------------------------
+''' 
+    A parosok_szama  nevű függvény,
+    visszatér egy számokat tartalmazó lista páros számainak számával.
+    
+    Üres lista esetén 0 a visszatérési érték.
+'''
+#Össze kell számolni a páros számokat
+def parosok_szama(parosok):
+    if(parosok == []):
+        return 0
+
+    par = 0#Összeszámoláshoz változó
+
+    for i in parosok: #Listán végiglépkedés
+        if (i % 2 == 0): #Eldöntés, hogy páros-e
+            par += 1 #Mivel páros, +1 a párosok számához
+    
+    return par #Válasz visszadobása
+
+#parosok_szama([-1, 5, 9, 8, 4564879564132, 3, 56])
+
+
+
+#--------------------------
+''' 
+    A negativok_szama nevű függvény,
+    paraméterként egy számokat tartalmazó listát kap és
+    visszatér egy számokat tartalmazó lista negativ számainak számával.
+    
+    Üres lista esetén 0 a visszatérési érték.
+'''
+def negativok_szama(negativ):
+    if (len(negativ) < 0):
+        return 0
+        
+
+
+#--------------------------
+'''
+    A benne_van_a_stringben nevű függvény.
+    első paraméterként egy stringet kap,
+    második paraméterként egy betüt.
+    Visszatérési értéke True, ha  a betü benne van a stringben.
+    A visszatérési érték False, ha  a betü nics benne a stringben.
+'''
+
+
+
+#--------------------------
+''' 
+    Az utolso_karakter nevű függvény,
+    paraméterként egy stringet kap és
+    visszatér az adott string utolso karakterével.
+    
+    Üres string esetén None a visszatérési érték.
+'''
+
+
+
+#--------------------------
+''' 
+    A pozitivok_szama nevű függvény,
+    paraméterként egy számokat tartalmazó listát kap és
+    visszatér egy számokat tartalmazó lista pozitív számainak számával.
+    
+    Üres lista esetén 0 a visszatérési érték.
+'''
+
+
+
+#--------------------------
+'''
+    A negativok_kivalogatasa nevű függvény,
+    visszatér egy listával, 
+      amely a paraméterként átadott számokat tartalmazó lista
+      negatív számait tartalmazza.
+'''
+
+
+
+#--------------------------
+''' 
+    Az elso_karakter nevű függvény,
+    paraméterként egy stringet kap és
+    visszatér a string első karakterével.
+    
+    Üres string esetén None a visszatérési érték.
+'''
+
+
+
+#--------------------------
+''' 
+    A pozitivok_kivalogatasa nevű függvény,
+    visszatér egy listával, 
+        amely a paraméterként átadott számokat tartalmazó lista
+        pozitiv számait tartalmazza. 
+'''
+
+
+
+#--------------------------
+'''
+    A faktorialis nevű függvény,
+    visszatér a paraméterként megkapott szám faktoriálisával.
+'''
+
+
+
+#--------------------------
+'''
+    A kereses_a_listaban nevű függvény.
+    Első bemeneti paraméter egy lista,
+    második bemeneti paraméter egy szám.
+    A visszatérési érték a paraméterként megadott szám
+        első előfordulási helye a listában.
+    
+    A visszatérési érték None, ha a szám nics benne a listában.
+'''
+
+
+
+#--------------------------
+''' 
+    A lista_atlag nevű függvény,
+    paraméterként egy számokat tartalmazó listát kap és
+    visszatér a lista számainak átlagával.
+'''
+
+
+
+#--------------------------
+''' 
+    A legnagyobb  nevű függvény,
+    paraméterként egy számokat tartalmazó listát kap és
+    visszatér a lista legnagyobb számával.
+    Üres lista esetén None a visszatérési érték.
+
+    A feladat megoldása során nem használhatod a max függvényt!
+'''
+
+
+
+#--------------------------
+''' 
+    A paratlanok_szama nevű függvény,
+    paraméterként egy számokat tartalmazó listát kap és
+    visszatér egy számokat tartalmazó lista páros számainak számával.
+    
+    Üres lista esetén 0 a visszatérési érték.
+'''
+
+
+
+#--------------------------
+''' 
+    Az osszeg  nevű függvény,
+    paraméterként egy számokat tartalmazó listát kap és
+    visszatér a lista számainak összegével.
+    Üres lista esetén 0 a visszatérési érték.
+    A feladat megoldása során nem használhatod a sum() függvényt!
+'''
+
+
+
+#--------------------------
+'''
+    A paratlanok_kivalogatasa nevű függvény,
+    visszatér egy listával, 
+        amely a paraméterként átadott számokat tartalmazó lista
+        páratlan számait tartalmazza.
+'''
+
+
+
+#--------------------------
+''' 
+    A szorzat  nevű függvény,
+    paraméterként egy számokat tartalmazó listát kap és
+    visszatér a lista számainak szorzatával.
+    
+    Üres lista esetén 1 a visszatérési érték.
+'''
+
+
+
+#--------------------------
+''' 
+    A legkisebb nevű függvény,
+    paraméterként egy számokat tartalmazó listát kap és
+    visszatér a lista legkisebb számával.
+    Üres lista esetén None a visszatérési érték.
+    
+    A feladat megoldása során nem használhatod a min() függvényt!
+'''
+
+
+
+#--------------------------
+'''
+    A benne_van_a_listaban nevű függvény,
+    első paraméterként egy listát kap,
+    második paraméterként egy számot.
+    A visszatérési érték True, ha  a szám benne van a listában.
+    A visszatérési érték False, ha  a szám nics benne a listában.
+    
+    Üres lista esetén a visszatérési érték False.
+'''
+
+
+
+#--------------------------
+'''
+    A parosok_kivalogatasa nevű függvény,
+    visszatér egy listával, 
+        amely a paraméterként átadott számokat tartalmazó lista
+        páros számait tartalmazza.
+'''
+
+
+
+#======================================================================================================================/home/tanarok/weszerle/Oktatas_2025-26/10K/Python/python2-gyak
